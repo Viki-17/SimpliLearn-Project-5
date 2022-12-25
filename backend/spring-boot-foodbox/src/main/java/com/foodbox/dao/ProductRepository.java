cb180a7e7b2b3962213ef71e1d783b91956ea415
Viki-17 <vigneshbusiness.r@gmail.com>
2022-12-25 18:34:12 +0530

    foodbox

---
+++ backend/spring-boot-foodbox/src/main/java/com/foodbox/dao/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.foodbox.dao;
+
+import com.foodbox.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam ("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
+}
